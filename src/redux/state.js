84d8747b77e6318d40d3c0258e76f9f708db79bb
Aleksandra <stormofe@gmail.com>
2021-06-26 14:47:34 +0300

    Lesson 33. Need return in future

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,4 @@
+import { rerenderEntireTree } from "../render";
 
 
 let state = {
@@ -40,6 +41,7 @@ export let addPost = (postMessage) => {
 	};
 
 	state.profilePage.posts.push(newPost);
+	rerenderEntireTree(state);
 }
 
 
