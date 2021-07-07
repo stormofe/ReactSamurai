8bc567f96fa9ae4dd1dd984ce72bb6c7e8fe8ded
Aleksandra <stormofe@gmail.com>
2021-07-07 11:39:58 +0300

    Refactor P-R and D-R

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -13,25 +13,28 @@ let initialState = {
 const profileReducer = (state = initialState, action) => {
 
 	switch(action.type) {
+		case UPDATE_NEW_POST_TEXT: {	
+				
+			return {
+				...state,
+				newPostText: action.newText
+			};
+		}
+
 		case ADD_POST:{
-				let newPost = {
+			let newPost = {
 				id: 5,
 				message: state.newPostText,
 				like: 0
 			};
 
-			let stateCopy = {...state};
-			stateCopy.posts = [...state.posts];
-			stateCopy.posts.push(newPost);
-			stateCopy.newPostText = '';
-			return stateCopy;
+			return {
+				...state,
+				newPostText: '',
+				posts: [...state.posts, newPost],
+			};
 		}
-		case UPDATE_NEW_POST_TEXT: {	
-				
-			let stateCopy = {...state};
-			stateCopy.newPostText = action.newText;
-			return stateCopy;
-		}
+
 		default: 
 		return state;
 	
