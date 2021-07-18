c6c584d3923ebcd722a72ecc5d3f1106695bc23c
Aleksandra <stormofe@gmail.com>
2021-07-18 10:28:02 +0300

    Less 58. Refactoring mapDispatchToProps

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,37 +1,51 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { followAC, setCurrentPageAC, setUsersAC, setUsersTotalCountAC, unfollowAC } from '../../redux/users-reducer';
+import {
+	follow,
+	setCurrentPage,
+	setUsers,
+	setUsersTotalCount,
+	toggleIsFetching,
+	unfollow
+} from '../../redux/users-reducer';
 import Users from './Users';
 import * as axios from 'axios';
-
+import Preloader from '../common/Preloader/Preloader';
 
 class UsersContainer extends React.Component {
 
 	componentDidMount() {
+		this.props.toggleIsFetching(true);
 		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
 			.then(response => {
+				this.props.toggleIsFetching(false);
 				this.props.setUsers(response.data.items);
-				this.props.setTotalUsersCount(response.data.totalCount);
+				this.props.setUsersTotalCount(response.data.totalCount);
 			});
 
 	}
 	onPageChanged = (pageNumber) => {
 		this.props.setCurrentPage(pageNumber);
+		this.props.toggleIsFetching(true);
 		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
 			.then(response => {
+				this.props.toggleIsFetching(false);
 				this.props.setUsers(response.data.items);
 
 			});
 	}
 
 	render() {
-		return <Users totalUsersCount={this.props.totalUsersCount}
-			pageSize={this.props.pageSize}
-			currentPage={this.props.currentPage}
-			onPageChanged={this.onPageChanged}
-			users={this.props.users}
-			unfollow={this.props.unfollow}
-			follow={this.props.follow} />
+		return <>
+			{this.props.isFetching ? <Preloader /> : null}
+			<Users totalUsersCount={this.props.totalUsersCount}
+				pageSize={this.props.pageSize}
+				currentPage={this.props.currentPage}
+				onPageChanged={this.onPageChanged}
+				users={this.props.users}
+				unfollow={this.props.unfollow}
+				follow={this.props.follow} />
+		</>
 	}
 }
 
@@ -41,30 +55,12 @@ let mapStateToProps = (state) => {
 		pageSize: state.usersPage.pageSize,
 		totalUsersCount: state.usersPage.totalUsersCount,
 		currentPage: state.usersPage.currentPage,
-
-	}
-}
-let mapDispatchToProps = (dispatch) => {
-	return {
-		follow: (userId) => {
-			dispatch(followAC(userId));
-		},
-		unfollow: (userId) => {
-			dispatch(unfollowAC(userId));
-		},
-		setUsers: (users) => {
-			dispatch(setUsersAC(users));
-		},
-		setCurrentPage: (pageNumber) => {
-			dispatch(setCurrentPageAC(pageNumber));
-		},
-		setTotalUsersCount: (totalCount) => {
-			dispatch(setUsersTotalCountAC(totalCount));
-		},
+		isFetching: state.usersPage.isFetching,
 	}
 }
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
+export default connect(mapStateToProps,
+	{ follow, unfollow, setUsers, setCurrentPage, setUsersTotalCount, toggleIsFetching }
+)(UsersContainer);
 
 
---
+++ src/components/common/Preloader/Preloader.jsx
@@ -0,0 +1,11 @@
+import preloader from '../../../assets/images/Spin.svg';
+
+let Preloader = (props) => {
+	return (
+		<div>
+			<img src={preloader} />
+		</div>
+	)
+}
+
+export default Preloader;
